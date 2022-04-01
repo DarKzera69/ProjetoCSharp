ea92ca9ac93763fb1e6b24debec99f2c419428a3
DarKzera69 <jorgeluiz00671@gmail.com>
2022-03-31 21:00:07 -0300

    projeto CSharp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Estou Chegando pra ganhar 15k");
+        }
+    }
+}
